6198af1f66d12e8691ec9ba8214929a54ce5e68b
Ravi727570 <148647125+Ravi727570@users.noreply.github.com>
2025-09-26 00:25:12 +0530

    Update LoginPage.js

--- src/authentication_pages/LoginPage.js
+++ src/authentication_pages/LoginPage.js
@@ -1,16 +1,60 @@
+import { useState } from "react";
 import "./LoginPage.css";
+import {useNavigate} from "react-router-dom";
 
 const LoginPage=()=>{
+    const [form ,setForm]=useState({
+        email:"",
+        password:""
+    });
+    const navigate = useNavigate();
+
+    const API_KEY =  "AIzaSyCtwLDyIgie3wiULpZiBj8FP6cFbJ3QSqs";
+    const handleChange=(event)=>{
+        const{name,value}=event.target;
+        setForm({...form,[name]:value});
+    };
+
+    const handleSubmit=async(event)=>{
+        event.preventDefault();
+        try{
+            const response=await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`,
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                    email: form.email,
+                    password: form.password,
+                    returnSecureToken: true
+                    })
+                }
+                );
+            const data=await response.json()
+            if(!data){
+                alert("No data, please signup first");
+                return;
+                }
+                alert("login success");
+                localStorage.setItem("idToken", data.idToken);
+                localStorage.setItem("userEmail", form.email);
+                navigate("/expensePage");
+        }catch(error){
+            console.log("error");
+        }
+    };
+
     return(
         <div className="container">.
             <div className="page_body">
-                <div className="layout">
+                <div className="login_layout">
                     <h3 className="head_text">Login</h3>
-                    <form >
+                    <form onSubmit={handleSubmit} >
                         <input type="email"
                         id="email"
                         name="email"
                         placeholder="Email"
+                        value={form.email}
+                        onChange={handleChange}
                         className="email_field"
                         required
                         />
@@ -18,17 +62,17 @@ const LoginPage=()=>{
                         id="password"
                         name="password"
                         placeholder="Password"
+                        value={form.password}
+                        onChange={handleChange}
                         className="password_field"
                         required
                         />
-                        <div className="btn">.
-                        <button type="submit" className="login_btn">Login</button>
+                        <button type="submit" className="loginPage_btn">Login</button>
                         <a href="/" className="forgot_link">Forgot Password</a>
-                        </div>
                     </form>
                 </div>
             </div>
-            <button type="submit" className="Signup_button">Don't have an Account?Sign up</button>
+            <button type="button" className="Signup_button" onClick={()=>navigate("/signup")}>Don't have an Account?Sign up</button>
         </div>
     );
 };
