1d6b8f6b5214e00cb99665457c975d5b98abb10f
Ravi727570 <148647125+Ravi727570@users.noreply.github.com>
2025-09-25 21:49:59 +0530

    Update CompleteProfile.js

--- src/authentication_pages/CompleteProfile.js
+++ src/authentication_pages/CompleteProfile.js
@@ -1,6 +1,53 @@
 import "./CompleteProfile.css";
+import {useState} from "react";
+import { useNavigate } from "react-router-dom";
 
-const CompleteProfile=()=>{
+const CompleteProfile=({idToken})=>{
+    const [name, setName] = useState("");
+    const [photoURL, setPhotoURL] = useState("");
+
+    const navigate = useNavigate();
+    const API_KEY =  "AIzaSyCtwLDyIgie3wiULpZiBj8FP6cFbJ3QSqs";
+    const storedToken = localStorage.getItem("idToken") || idToken;
+    const resetdata=()=>{
+            setName("");
+            setPhotoURL("");
+    };
+
+    const handleCancel = () => {
+    navigate("/expensePage");
+  };
+    const handleUpdate=async(event)=>{
+        event.preventDefault();
+
+        try {
+      const response = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_KEY}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            idToken: storedToken,
+            displayName: name,
+            photoUrl: photoURL,
+            returnSecureToken: true
+          })
+        }
+      );
+      const data = await response.json();
+      if(!data){
+        alert("invalid data");
+        return;
+      }if (data.idToken) {
+        localStorage.setItem("idToken", data.idToken);
+      }
+      alert("Profile updated successfully!");
+      resetdata();
+    } catch (error) {
+      alert("Error updating profile: ;.");
+    }
+    };
+
     return(
         <div className="profileContainer">
             <div className="profile_layout">
@@ -10,16 +57,18 @@ const CompleteProfile=()=>{
             <hr/>
             <div className=" complete_profile_headText">
                 <h3 className="text">Contact Details</h3>
-                <button type="button" className="cancel_btn">Cancel</button>
+                <button type="button" className="cancel_btn" onClick={handleCancel}>Cancel</button>
             </div>
-            <form>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" />
-                <label htmlFor="name">Profile Photo URL</label>
-                <input type="text" id="name" name="name" />
-                <button type="submit" className="update_btn">Update</button>
-            </form>
-            <hr/>
+            <div className="formDetails">
+                <form onSubmit={handleUpdate}>
+                    <label htmlFor="name">Full Name</label>
+                    <input type="text" id="name" name="name" className="input" value={name} onChange={(event)=>setName(event.target.value)} />
+                    <label htmlFor="photo">Profile Photo URL</label>
+                    <input type="text" id="photo" name="photo" className="input"value={photoURL} onChange={(event)=>setPhotoURL(event.target.value)} />
+                    <button type="submit" className="update_btn">Update</button>
+                    <hr/>
+                </form>
+            </div>
         </div>
     );
 };
